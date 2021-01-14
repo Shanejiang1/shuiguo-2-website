479b5db17c79b2d286ea673b7dde4804d9b6b112
Shanejiang1 <jiangshanghang@gmail.com>
2021-01-14 21:10:26 +0800

    deploy

---
+++ precache-manifest.642ee32ac795715f5d5a8b546da4d3ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7a5b6a764936d0391a",
+    "url": "/shuiguo-2-website/css/app.4dcb6b54.css"
+  },
+  {
+    "revision": "ac7d9b30bc46ac175e04e56a94a30b4c",
+    "url": "/shuiguo-2-website/index.html"
+  },
+  {
+    "revision": "6e7a5b6a764936d0391a",
+    "url": "/shuiguo-2-website/js/app.328f7aa6.js"
+  },
+  {
+    "revision": "ced6c8664c9724f193ab",
+    "url": "/shuiguo-2-website/js/chunk-vendors.1f7f9065.js"
+  },
+  {
+    "revision": "ad4d4216d15fbe309cf33a2130894315",
+    "url": "/shuiguo-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shuiguo-2-website/robots.txt"
+  }
+]);
